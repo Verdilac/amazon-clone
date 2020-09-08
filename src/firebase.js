848601efafa32694716a69c803009d79fa5cca48
Verdilac <lathindu2000@gmail.com>
2020-09-09 03:07:22 +0530

    first

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-_qe1tZLXJccbNztYjMaibYX3G3Z1s_0",
+  authDomain: "fir-84937.firebaseapp.com",
+  databaseURL: "https://fir-84937.firebaseio.com",
+  projectId: "fir-84937",
+  storageBucket: "fir-84937.appspot.com",
+  messagingSenderId: "692022410928",
+  appId: "1:692022410928:web:0eaaa6f338b22b5cc80575",
+  measurementId: "G-0GPLFQ26VT",
+};
