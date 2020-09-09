4f24b764b1edb6e40119ea776ec1e93b4e13374a
Verdilac <lathindu2000@gmail.com>
2020-09-09 21:03:58 +0530

    untill reducer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <h1></h1>
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
