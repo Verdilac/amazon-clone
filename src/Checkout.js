5b667fc625d2deaa3ef9c859403b07d098df75cc
Verdilac <lathindu2000@gmail.com>
2020-09-10 12:33:12 +0530

    ready to implement remove id for the basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+
 function Checkout() {
+  const [{basket},dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,7 +18,34 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          {/* bascket item */}
+          <CheckoutProduct
+              id='10948'
+              title='test,this is a test this is a test this is a test this is a test this is a test this is a test this is a test'
+              image='https://pbs.twimg.com/profile_images/717759633356623872/YrCHpa0u_400x400.jpg'
+              price={239.99}
+              rating={5}
+            />
+            <CheckoutProduct
+              id='10948'
+              title='test,this is a test this is a test this is a test this is a test this is a test this is a test this is a test'
+              image='https://pbs.twimg.com/profile_images/717759633356623872/YrCHpa0u_400x400.jpg'
+              price={239.99}
+              rating={5}
+            />
+
+
+
+
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
           {/* bascket item */}
           {/* bascket item */}
         </div>
--- src/reducer.js
+++ src/reducer.js
@@ -4,18 +4,23 @@ export const initialState = {
 
 //selector
 export const getBasketTotal = (basket) =>
-basket?.reduce((amount,item) => item.price + amount,0);
+  basket?.reduce((amount, item) => item.price + amount, 0);
 //using reduce to itterate over the basket and tally up item prices and added them  the amount
 //and we setup the initial value of amount to 0
-//optional chaining (?) is added to save undefined problems 
+//optional chaining (?) is added to save undefined problems
 const reducer = (state, action) => {
-    console.log(action);
+  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let
 
     default:
       return state;
