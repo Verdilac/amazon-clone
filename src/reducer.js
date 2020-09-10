362aef247aad204b1a125742f87595469e5efeb5
Verdilac <lathindu2000@gmail.com>
2020-09-10 11:03:32 +0530

    Day 2 Home Work Done and Dusted

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,12 @@ export const initialState = {
   basket: [],
 };
 
+//selector
+export const getBasketTotal = (basket) =>
+basket?.reduce((amount,item) => item.price + amount,0);
+//using reduce to itterate over the basket and tally up item prices and added them  the amount
+//and we setup the initial value of amount to 0
+//optional chaining (?) is added to save undefined problems 
 const reducer = (state, action) => {
     console.log(action);
   switch (action.type) {
